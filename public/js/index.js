a77a671e0e17b77119d43553daa4f09161f60aee
dhirajs16 <sarrafd16@gmail.com>
2024-02-16 09:35:29 +0545

    completed homepage with authentication

--- public/js/index.js
+++ public/js/index.js
@@ -80,7 +80,35 @@ home.addEventListener("mouseover", (coordinates) => {
 })
 
 
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    centeredSlides: false,
+    slidesPerGroupSkip: 1,
+    grabCursor: true,
+    keyboard: {
+      enabled: true,
+    },
+    breakpoints: {
+      769: {
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+      },
+    },
+    scrollbar: {
+      el: ".swiper-scrollbar",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+
 const year = new Date().getFullYear();
-console.log(year);
 
 document.querySelector("#year").innerText = year;
